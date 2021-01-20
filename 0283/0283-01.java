221f77c87cf2b5cdfc8b2a8b7225a1ec18eebd18
Cass Hsu <cass.hsu@gmail.com>
2021-01-20 23:30:03 +0800

    init 283 & Approach 1

---
+++ 0283/0283-01.java
@@ -0,0 +1,15 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int p = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0) {
+                if (p != i) {
+                    int tmp = nums[i];
+                    nums[i] = nums[p];
+                    nums[p] = tmp;
+                }
+                p++;
+            }
+        }
+    }
+}
