416b0d26e3b4c9dae042f11540f9d024f1d63387
CassHsu <cass.hsu@gmail.com>
2021-04-26 13:50:32 +0800

    Approach 3: Fibonacci Number

---
+++ 0070/0070-03.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int climbStairs(int n) {
+        if (n == 1) return 1;
+        
+        int prev = 1;
+        int curr = 2;
+        for (int i = 3; i <= n; i++) {
+            int tmp = prev + curr;
+            prev = curr;
+            curr = tmp;
+        }
+        return curr;
+    }
+}
