e8461cf812b59faa9d0c1620d8bc547dbfd79548
CassHsu <cass.hsu@gmail.com>
2021-05-24 10:20:40 +0800

    Approach 1: DP - Rescursion

---
+++ 0096/0096-01.java
@@ -0,0 +1,15 @@
+class Solution {
+    HashMap<Integer, Integer> cache = new HashMap<>();
+    
+    public int numTrees(int n) {
+        if (n == 0) return 1;
+        if (cache.containsKey(n)) return cache.get(n);
+        
+        int ans = 0;
+        for (int i = 1; i <= n; i++) {
+            ans += numTrees(i - 1) * numTrees(n - i);
+        }
+        cache.put(n, ans);
+        return ans;
+    }
+}
