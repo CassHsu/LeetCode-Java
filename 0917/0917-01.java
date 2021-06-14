9739018cdf2dc7c61e378e3a15a065a441d43278
CassHsu <cass.hsu@gmail.com>
2021-06-14 11:38:13 +0800

    Approach 1: Two Pointers

---
+++ 0917/0917-01.java
@@ -0,0 +1,17 @@
+class Solution {
+    public String reverseOnlyLetters(String s) {
+        StringBuilder sb = new StringBuilder();
+        int j = s.length() - 1;
+        for (int i = 0; i < s.length(); i++) {
+            if (Character.isLetter(s.charAt(i))) {
+                while (!Character.isLetter(s.charAt(j))) {
+                    j--;
+                }
+                sb.append(s.charAt(j--));
+            } else {
+                sb.append(s.charAt(i));
+            }
+        }
+        return sb.toString();
+    }
+}
