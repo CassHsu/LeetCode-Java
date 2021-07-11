46bb50dc9166202a8ad71bb8cde441455089209c
CassHsu <cass.hsu@gmail.com>
2021-07-11 22:58:42 +0800

    Create 0441-02.java

---
+++ 0441/0441-02.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int arrangeCoins(int n) {
+        int k = 1;
+        
+        while (n >= k) {
+            n -= k;
+            k++;
+        }
+        
+        return k - 1;
+    }
+}
