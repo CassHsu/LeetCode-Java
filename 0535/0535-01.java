7935a04672d836df0bb7920119f6daf9fb0b7ba5
CassHsu <cass.hsu@gmail.com>
2022-04-24 11:44:39 +0800

    Map

---
+++ 0535/0535-01.java
@@ -0,0 +1,30 @@
+public class Codec {
+    private String alphabet = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    private HashMap<String, String> map = new HashMap<>();
+    private Random rand = new Random();
+    private String key;
+    
+    private String getRand() {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < 6; i++) {
+            sb.append(alphabet.charAt(rand.nextInt(62)));
+        }
+        
+        return sb.toString();
+    }
+    
+    // Encodes a URL to a shortened URL.
+    public String encode(String longUrl) {
+        key = getRand();
+        while (map.containsKey(key)) {
+            key = getRand();
+        }
+        map.put(key, longUrl);
+        return "http://tinyurl.com/" + key;
+    }
+
+    // Decodes a shortened URL to its original URL.
+    public String decode(String shortUrl) {
+        return map.get(shortUrl.replace("http://tinyurl.com/", ""));
+    }
+}
