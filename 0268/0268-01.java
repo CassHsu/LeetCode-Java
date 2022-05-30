6303c0708ce678d5618fdb85998018efcc7ffb52
CassHsu <cass.hsu@gmail.com>
2022-05-30 21:37:12 +0800

    Bit Manipulation

---
+++ 0268/0268-01.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+        int missing = nums.length;
+        
+        for (int i = 0; i < nums.length; i++) {
+            var t = i xor nums[i];
+						miss
+        }
+        
+        return missing;
+    }
+}
