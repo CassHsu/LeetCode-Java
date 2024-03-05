dfd61e29cf8c644dd272c20119c575719261c77b
CassHsu <cass.hsu@gmail.com>
2024-03-05 10:56:18 +0800

    Create 2828-01.java

---
+++ 2828/2828-01.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean isAcronym(List<String> words, String s) {
+        StringBuffer sb = new StringBuffer();
+
+        for (String w: words) {
+            sb.append(w.charAt(0));
+        }
+
+        return s.equals(sb.toString());
+    }
+}
