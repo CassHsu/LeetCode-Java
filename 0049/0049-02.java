204ab3e6a223654650cab06c0342e1c7b4c7e990
CassHsu <cass.hsu@gmail.com>
2024-09-28 23:39:47 +0800

    Create 0049-02.java

---
+++ 0049/0049-02.java
@@ -0,0 +1,15 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        HashMap<String, List<String>> map = new HashMap<>();
+        
+        for (String str : strs) {
+            char[] charArray = str.toCharArray();
+            java.util.Arrays.sort(charArray);
+            String sortedStr = new String(charArray);
+            map.putIfAbsent(sortedStr, new ArrayList<>());
+            map.get(sortedStr).add(str);
+        }
+        
+        return new ArrayList<>(map.values());
+    }
+}
