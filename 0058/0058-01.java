4b58e80e7378ee95d190076507d7b24a5d70e6ac
CassHsu <cass.hsu@gmail.com>
2024-10-11 22:53:50 +0800

    Create 0058-01.java

---
+++ 0058/0058-01.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int lengthOfLastWord(String s) {
+        String trimmed = s.trim();
+        int lastSpaceIndex = trimmed.lastIndexOf(' ');
+        
+        return trimmed.length() - lastSpaceIndex - 1;
+    }
+}
